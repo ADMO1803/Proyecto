f14bdcbbf8ca5adda8773539b5cb57bbad65107c
ADMO1803 <angeldavidm1903@gmail.com>
2023-03-28 15:16:18 -0400

    pantallas-listas

--- js/script.js
+++ js/script.js
@@ -10,5 +10,30 @@ AOS.init();
 =            MENU            =
 ====================================*/
 
+const menu = document.querySelector("#menu-icon");
+const menunav = document.querySelector(".menu-nav");
+
+menu.addEventListener("click", () => {
+  menunav.classList.toggle("active")
+})
+
+menunav.addEventListener("click", () => {
+  menunav.classList.remove("active")
+})
+
 /*=====  End of MENU ======*/
 
+/*====================================
+=    BOTON-ENVIAR SWEET ALERT            =
+====================================*/
+
+function enviar() {
+	// body...
+	Swal.fire(
+  'Enviado!',
+  'Su mensaje fue enviado!',
+  'success'
+  )
+}
+
+/*=====  End of BOTON-ENVIAR ======*/
